735b6bfd198f193f4056d4e8472bf21b7a97fb3d
vinothsmart <s.vinothkanna90@gmail.com>
2022-06-23 15:23:47 +0530

    get methid

--- index.js
+++ index.js
@@ -3,6 +3,10 @@ const express = require("express");
 // set up express app
 const app = express();
 
+app.get("/", () => {
+  console.log("GET request to the homepage");
+});
+
 // listen for requests
 app.listen(process.env.port || 4000, () => {
   console.log("Example app listening on port 4000!");
